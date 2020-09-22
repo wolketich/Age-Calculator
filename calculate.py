05994dbb454382598eaf404470ce814c844aeb90
Vladislav Cernega <vladislav@walkcat.net>
2020-09-23 00:00:00 +0100

    Commit 2020-09-23 - 1 files updated.

--- calculate.py
+++ calculate.py
@@ -6,3 +6,11 @@ def judge_leap_year(year):
         return True
     else:
         return False
+
+def month_days(month, leap_year):
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        return 31
+    elif month in [4, 6, 9, 11]:
+        return 30
+    elif month == 2 and leap_year:
+        return 29
